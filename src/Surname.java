8e718d9a99241b0c4cedf159eb58486391d287d8
Jetzko <andreadefilippi68@gmail.com>
2022-12-22 15:33:31 +0100

    If 02

---
+++ src/Surname.java
@@ -0,0 +1,10 @@
+public class Surname {
+    public static void main(String[] args) {
+        String surname = "De Filippi";
+        if (surname.contains("in")){
+            System.out.println("Your surname contains the sequence of letters 'in'");
+        } else{
+            System.out.println("Your surname doesn't contains the sequence of letters 'in'");
+        }
+    }
+}
